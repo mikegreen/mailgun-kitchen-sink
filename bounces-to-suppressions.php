f2bd30e3c16ac990ec081237e945b22cfc153a8d
Mike Green <772413+mikegreen@users.noreply.github.com>
2023-12-31 16:41:02 -0700

    handle catching up and errors

--- bounces-to-suppressions.php
+++ bounces-to-suppressions.php
@@ -11,48 +11,104 @@ include "settings.php";
 // $sendingDomain  
 
 $mailgun = Mailgun::create($mailgunApiKey, $mailgunDomain);
-
 $bouncesAdded = 0;
+$secondsInDay = 86400;
+$timeOffsetIncrement = 7200; // 2 hours
+$beginTime = microtime(true) - $timeOffsetIncrement;
+$endTime = microtime(true);
+$daysToGoBack = 2;
 
 print(microtime(true) . "\n");
 
-print("Domain: " . $sendingDomain . "\n");
+$i = 0;
 
-// Get events
-$response = $mailgun->events()->get($sendingDomain, [
-    'event' => 'failed',
-    'severity' => 'permanent',
-    'begin' => microtime(true) - 43200,
-    'end' => microtime(true),
-    'limit' => 300
-]);
+while ($beginTime > (microtime(true) - ($daysToGoBack * $secondsInDay))) {
+    print("Domain: " . $sendingDomain . " from " . date('r', (int)$beginTime) . " to " . date('r',(int)$endTime) . "\n");
+    print("Starting run " . $i . "\n");
 
-// print_r($response);
+    // Get events
+    $response = getFailedEvents($mailgun, $sendingDomain, $beginTime, $endTime);
 
-// Set variable with the number of records returned by $response
-$recordCount = count($response->getItems());
-print("Number of records: " . $recordCount . "\n");
+    // Set variable with the number of records returned by $response
+    $recordCount = count($response->getItems());
+    print("Number of records: " . $recordCount . "\n");
 
-// Print recipient from $response and add to bounce list in Mailgun
-foreach ($response->getItems() as $item) {
-    $failedRecipient = $item->getRecipient();
-    echo "Recipient: " . $failedRecipient . "\n";
-    $failedDeliveryStatus = $item->getDeliveryStatus();
-    $failedCode = $failedDeliveryStatus['code'] . "\n";
-    $failedMessage = $failedDeliveryStatus['message'] . "\n";
-    
-    // blindly insert bounces as we can't search if it exists until this 
-    //   bug is resolved https://github.com/mailgun/mailgun-php/issues/887
-    $result = $mailgun->suppressions()->bounces()->create(
-        $sendingDomain, $failedRecipient, ['error' => $failedMessage, 'code' => $failedCode]
-    );
+    processBounces($mailgun, $sendingDomain, $response);
 
-    print_r($result);
+    $beginTime = $beginTime - $timeOffsetIncrement;
+    $endTime = $endTime - $timeOffsetIncrement;
 
-    $bouncesAdded++;
-    print("Bounces added: " . $bouncesAdded . "\n");
+    $i++;
 }
-    
+
+function getFailedEvents($mailgun, $sendingDomain, $beginTime, $endTime) {
+    $response = $mailgun->events()->get($sendingDomain, [
+        'event' => 'failed',
+        'severity' => 'permanent',
+        'begin' => $beginTime,
+        'end' => $endTime,
+        'limit' => 300
+    ]);
+
+    return $response;
+}
+
+function processBounces($mailgun, $sendingDomain, $response) {
+    $bouncesAdded = 0;
+
+    // Print recipient from $response and add to bounce list in Mailgun
+    foreach ($response->getItems() as $item) {
+        $failedRecipient = $item->getRecipient();
+        echo "Check if bounce already exists for: " . $failedRecipient . "\n";
+
+        $bounceExists = bounceExists($mailgun, $sendingDomain, $failedRecipient);
+
+        if($bounceExists) {
+            echo "Bounce already exists for: " . $failedRecipient . "\n";
+            continue;
+        } else {
+            echo "Bounce does not exist for: " . $failedRecipient . "\n";
+
+            $failedDeliveryStatus = $item->getDeliveryStatus();
+            $failedCode = $failedDeliveryStatus['code'] . "\n";
+            $failedMessage = $failedDeliveryStatus['message'] . "\n";
+            $failedTimestamp = $item->getTimestamp();
+            $failedDate = date('r', $failedTimestamp);
+
+            // print_r($item);
+            
+            echo "Recipient: " . $failedRecipient . "\n";
+            echo "Error : " . $failedMessage . "code: " . $failedCode . "on " . $failedDate . "\n";
+
+            $result = $mailgun->suppressions()->bounces()->create(
+                $sendingDomain, $failedRecipient, [
+                        'error' => $failedMessage, 
+                        'code' => $failedCode, 
+                        'created_at' => $failedDate
+                        ]
+            );
+
+            // print_r($result);
+
+            $bouncesAdded++;
+            print("Bounces added: " . $bouncesAdded . "\n");
+
+        }
+    }
+
+    return $bouncesAdded;
+}
+
+function bounceExists($mailgun, $sendingDomain, $failedRecipient) {
+    try {
+        $responseBounce = $mailgun->suppressions()->bounces()->show($sendingDomain, $failedRecipient);
+        return true;
+    } catch (\Exception $e) {
+        // https://github.com/mailgun/mailgun-php/issues/887
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+        return false;
+    }
+}
 
     // $responseBounces = $mailgun->suppressions()->bounces()->index($sendingDomain);
 
@@ -62,4 +118,4 @@ foreach ($response->getItems() as $item) {
     //     echo "Address: " . $bounce->getAddress() . "\n";
     // }
      
-    ?>
+?> 
